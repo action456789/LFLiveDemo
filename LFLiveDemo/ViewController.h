36159f5dd7de6e7b9e7b9798cb260edbcb5a279d
KeSen <superkesen@gmail.com>
2017-08-08 11:14:15 +0800

    first commit

---
+++ LFLiveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFLiveDemo
+//
+//  Created by sen.ke on 2017/8/8.
+//  Copyright © 2017年 sen.ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LFLiveDemo/ViewController.m
@@ -0,0 +1,129 @@
+//
+//  ViewController.m
+//  LFLiveDemo
+//
+//  Created by sen.ke on 2017/8/8.
+//  Copyright © 2017年 sen.ke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <LFLiveKit.h>
+
+@interface ViewController ()<LFLiveSessionDelegate>
+
+@property (nonatomic, strong) LFLiveSession *session;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (LFLiveSession*)session {
+    if (!_session) {
+        _session = [[LFLiveSession alloc] initWithAudioConfiguration:[LFLiveAudioConfiguration defaultConfiguration] videoConfiguration:[LFLiveVideoConfiguration defaultConfiguration]];
+        _session.captureDevicePosition = AVCaptureDevicePositionBack;
+        _session.delegate = self;
+        _session.running = YES;
+        _session.preView = self.view;
+    }
+    return _session;
+}
+
+- (void)startLive {
+    // 判断是否有摄像头
+    if(![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        NSLog(@"您的设备没有摄像头或者相关的驱动, 不能进行直播");
+        return;
+    }
+    
+    // 判断是否有摄像头权限
+    AVAuthorizationStatus  authorizationStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    if (authorizationStatus == AVAuthorizationStatusRestricted|| authorizationStatus == AVAuthorizationStatusDenied) {
+        NSLog(@"app需要访问您的摄像头。\n请启用摄像头-设置/隐私/摄像头");
+        return;
+    }
+    
+    // 开启麦克风权限
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    if ([audioSession respondsToSelector:@selector(requestRecordPermission:)]) {
+        [audioSession performSelector:@selector(requestRecordPermission:) withObject:^(BOOL granted) {
+            if (granted) {
+                return YES;
+            }
+            else {
+                //[self showInfo:@"app需要访问您的麦克风。\n请启用麦克风-设置/隐私/麦克风"];
+                return NO;
+            }
+        }];
+    }
+    
+    LFLiveStreamInfo *streamInfo = [LFLiveStreamInfo new];
+    streamInfo.url = @"rtmp://192.168.0.50:1935/rtmplive/room";
+    [self.session startLive:streamInfo];
+}
+
+- (void)stopLive {
+    if (self.session.state == LFLivePending || self.session.state == LFLiveStart){
+        [self.session stopLive];
+    }
+}
+
+//MARK: - CallBack:
+- (void)liveSession:(nullable LFLiveSession *)session liveStateDidChange: (LFLiveState)state {
+    NSString *tempStatus;
+    switch (state) {
+        case LFLiveReady:
+            tempStatus = @"准备中";
+            break;
+        case LFLivePending:
+            tempStatus = @"连接中";
+            break;
+        case LFLiveStart:
+            tempStatus = @"已连接";
+            break;
+        case LFLiveStop:
+            tempStatus = @"已断开";
+            break;
+        case LFLiveError:
+            tempStatus = @"连接出错";
+            break;
+        default:
+            break;
+    }
+    NSLog(@"%@", tempStatus);
+}
+
+- (void)liveSession:(nullable LFLiveSession *)session debugInfo:(nullable LFLiveDebug*)debugInfo {
+    NSLog(@"%@", debugInfo);
+}
+
+- (void)liveSession:(nullable LFLiveSession*)session errorCode:(LFLiveSocketErrorCode)errorCode {
+    NSLog(@"%lu", (unsigned long)errorCode);
+}
+
+
+- (IBAction)startAction:(id)sender {
+    [self startLive];
+}
+
+- (IBAction)stopAction:(id)sender {
+    [self stopLive];
+}
+
+- (IBAction)switchCamare:(id)sender {
+    AVCaptureDevicePosition devicePositon = self.session.captureDevicePosition;
+    self.session.captureDevicePosition = (devicePositon == AVCaptureDevicePositionBack) ? AVCaptureDevicePositionFront : AVCaptureDevicePositionBack;
+    NSLog(@"切换前置/后置摄像头");
+}
+@end
